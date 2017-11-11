bc97d635e962297bcd58eff097b64e4b64271d30
Marc Anderson <marcanderson@Marcs-MacBook-Pro.local>
2017-11-11 18:29:40 -0500

    Added image assets and updated to css grid instead of flexbox. Also added html5 tags

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(window).bind('scroll',function(e){
 
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
-    $('.parallax-bg1').css('bottom',(0-(scrolled*.1))+'px');
-    $('.parallax-bg2').css('bottom',(0-(scrolled*.15))+'px');
+    $('.parallax-bg1').css('bottom',(0-(scrolled*.1 + 0))+'px');
+    $('.parallax-bg2').css('bottom',(0-(scrolled*.15 + 17))+'px');
+    $('.parallax-bg3').css('bottom',(0-(scrolled*.12 + 0))+'px');
 }
