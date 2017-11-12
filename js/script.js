4b58351c5b8886ee9aa790550d3851b99306eb98
Marc Anderson <marcanderson@Marcs-MacBook-Pro.local>
2017-11-12 00:23:49 -0500

    more details added

--- js/script.js
+++ js/script.js
@@ -7,4 +7,7 @@ function parallaxScroll(){
     $('.parallax-bg1').css('bottom',(0-(scrolled*.1 + 0))+'px');
     $('.parallax-bg2').css('bottom',(0-(scrolled*.15 + 17))+'px');
     $('.parallax-bg3').css('bottom',(0-(scrolled*.12 + 0))+'px');
+    $('.parallax-bg4').css('bottom',(0-(scrolled*.06 + 17))+'px');
+    $('.parallax-bg5').css('bottom',(0-(scrolled*.03 + 34))+'px');
+    $('.parallax-bg6').css('bottom',(0-(scrolled*.07 + 51))+'px');
 }
