8552e851dd1b078620b17a0219481aa1cdf5df60
Marc Anderson <marcanderson@Marcs-MacBook-Pro-2.local>
2017-11-15 10:34:32 -0500

    got ScrollMagic working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-$(function (){ // Wait for document ready
-    // Init controller
-    var controller = new ScrollMagic.Controller();
+$(function() { // wait for document ready
+  // init controller
+  var controller = new ScrollMagic.Controller();
 
-    // Build scene
-    new ScrollMagic.Scene({
-        triggerElement: "#projects", // point of execution
-        duration: $(window).height(), // pin element for the window height
-        triggerHook: 0, // don't trigger until #projects hits the top of the viewport
-        reverse: true // allows the effect to trigger when srolled in reverse direction
-    })
-    .setPin("#target") // the element we want to point
-    .addTo(controller); // add scene to ScrollMagic controller
+  // build tween
+  var tween = TweenMax.to("#pinned-target1", 1, {
+    rotation: 360,
+    ease: Linear.easeNone
+  });
+
+  // build scene
+  var scene = new ScrollMagic.Scene({triggerElement: "#projects", duration: 300}).setTween(tween).setPin("#pinned-target1", {pushFollowers: false}).addIndicators(). // add indicators (requires plugin)
+  addTo(controller);
 });
