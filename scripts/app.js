7cdd3c441428715ae06103d6b7fc2fb05a8d4957
jaforewit <marcda495@gmail.com>
2018-03-23 10:52:24 -0400

    created addModel script

--- scripts/app.js
+++ scripts/app.js
@@ -30,6 +30,85 @@ var Init = function () {
 	});
 };
 
+var addModel = function(gl, program, textureImage, modelObj) {
+	//
+	// Create buffer
+	//
+	var vertices = modelObj.meshes[0].vertices;
+	var indices = [].concat.apply([], modelObj.meshes[0].faces);
+	var texCoords = modelObj.meshes[0].texturecoords[0];
+	var normals = modelObj.meshes[0].normals;
+
+	var posVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, posVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	var texCoordVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+
+	var indexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+	var normalBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, posVertexBufferObject);
+	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+	gl.vertexAttribPointer(
+		positionAttribLocation, // Attribute location
+		3, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0 // Offset from the beginning of a single vertex to this attribute
+	);
+	gl.enableVertexAttribArray(positionAttribLocation);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordVertexBufferObject);
+	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+	gl.vertexAttribPointer(
+		texCoordAttribLocation, // Attribute location
+		2, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0
+	);
+	gl.enableVertexAttribArray(texCoordAttribLocation);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, normalBufferObject);
+	var normalAttribLocation = gl.getAttribLocation(program, 'vertNormal');
+	gl.vertexAttribPointer(
+		normalAttribLocation,
+		3, gl.FLOAT,
+		gl.TRUE,
+		3 * Float32Array.BYTES_PER_ELEMENT,
+		0
+	);
+	gl.enableVertexAttribArray(normalAttribLocation);
+
+	//
+	// Create texture
+	//
+	var texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(
+		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		textureImage
+	);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+	return([texture, indices]);
+};
+
 var Run = function (vertexShaderText, fragmentShaderText, leafImage, leafModel) {
 	console.log('This is working');
 	model = leafModel;
@@ -94,80 +173,12 @@ var Run = function (vertexShaderText, fragmentShaderText, leafImage, leafModel) 
 	}
 
 	//
-	// Create buffer
+	// Add model
 	//
-	var leafVertices = leafModel.meshes[0].vertices;
-	var leafIndices = [].concat.apply([], leafModel.meshes[0].faces);
-	var leafTexCoords = leafModel.meshes[0].texturecoords[0];
-	var leafNormals = leafModel.meshes[0].normals;
+	var modelInfo = addModel(gl, program, leafImage, leafModel);
+	leafTexture = modelInfo[0];
+	leafIndices = modelInfo[1];
 
-	var leafPosVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafPosVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(leafVertices), gl.STATIC_DRAW);
-
-	var leafTexCoordVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafTexCoordVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(leafTexCoords), gl.STATIC_DRAW);
-
-	var leafIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, leafIndexBufferObject);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(leafIndices), gl.STATIC_DRAW);
-
-	var leafNormalBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafNormalBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(leafNormals), gl.STATIC_DRAW);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafPosVertexBufferObject);
-	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	gl.vertexAttribPointer(
-		positionAttribLocation, // Attribute location
-		3, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0 // Offset from the beginning of a single vertex to this attribute
-	);
-	gl.enableVertexAttribArray(positionAttribLocation);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafTexCoordVertexBufferObject);
-	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
-	gl.vertexAttribPointer(
-		texCoordAttribLocation, // Attribute location
-		2, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0
-	);
-	gl.enableVertexAttribArray(texCoordAttribLocation);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, leafNormalBufferObject);
-	var normalAttribLocation = gl.getAttribLocation(program, 'vertNormal');
-	gl.vertexAttribPointer(
-		normalAttribLocation,
-		3, gl.FLOAT,
-		gl.TRUE,
-		3 * Float32Array.BYTES_PER_ELEMENT,
-		0
-	);
-	gl.enableVertexAttribArray(normalAttribLocation);
-
-	//
-	// Create texture
-	//
-	var leafTexture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, leafTexture);
-	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texImage2D(
-		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		leafImage
-	);
-	gl.bindTexture(gl.TEXTURE_2D, null);
 
 	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
